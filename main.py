2757d3cc1b5fa1f5a5bf146a623229624cfd8e14
msternbach <123423284+msternbach@users.noreply.github.com>
2024-02-15 14:31:53 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+import pygame_menu
+from src.controller import Controller
+
+
+def main():
+    controller = Controller()
+    controller.mainloop()
+
+    
+if __name__ == '__main__':
+    main()
